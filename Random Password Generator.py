65883a53c8888cd29a4d999c3beea4a1b4ddf125
Ugonna <82211473+AmethystCane@users.noreply.github.com>
2021-04-22 19:26:35 +0100

    Add files via upload

---
+++ Random Password Generator.py
@@ -0,0 +1,55 @@
+import random
+from random import randint
+
+# All uppercase password
+password = ""
+for i in range(10):                       # 10 character password
+    a = chr(randint(65, 90))              # letter A - Z (uppercase)
+    password = str(password) + a          # Take the empty string and add a random letter 10x
+print(password)
+
+
+# Uppercase and lowercase password
+password = ""
+for i in range(5):                        # 5 upper case characters
+    a = chr(randint(65, 90))
+    b = chr(randint(65, 90)).lower()      # 5 lowercase characters
+    password = str(password) + a + b
+print(password)
+
+
+# Uppercase, lowercase and integer password
+password = ""
+for i in range(3):
+    a = chr(randint(65, 90))
+    b = chr(randint(65, 90)).lower()
+    c = str(randint(0, 9))
+    password = str(password) + a + b + c
+print(password)
+
+
+# Upper, lowercase and numbers password then shuffled
+password = ""
+for i in range(3):
+    a = chr(randint(65, 90))
+    b = chr(randint(65, 90)).lower()
+    c = str(randint(0, 9))
+    password = str(password) + a + b + c
+
+list1 = random.sample(password, len(password))  # stores lists
+shuffled_password = "".join(list1)              # turns list to string
+print(shuffled_password)                        # prints new shuffled password
+
+
+# Upper, lowercase, numbers and special characters then shuffled
+password = ""
+for i in range(3):
+    a = chr(randint(65, 90))
+    b = chr(randint(65, 90)).lower()
+    c = str(randint(0, 9))
+    d = chr(randint(*random.choice([(33, 47), (58, 64), (91, 96), (123, 126)])))
+    password = str(password) + a + b + c + d
+
+list1 = random.sample(password, len(password))
+shuffled_password = "".join(list1)
+print(shuffled_password)
